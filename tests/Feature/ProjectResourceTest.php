ae197171f5b8e99944c28e4bbac7a36cbe1c2e0b
hasan.tahseen <hasan.tahseen@ta3leem.dev>
2025-05-17 09:10:01 +0300

    using livewire

--- tests/Feature/ProjectResourceTest.php
+++ tests/Feature/ProjectResourceTest.php
@@ -2,22 +2,21 @@
 
 use Filament\Facades\Filament;
 use App\Filament\Admin\Resources\ProjectResource;
+use App\Filament\Admin\Resources\ProjectResource\Pages\ListProjects;
 use App\Models\Project;
 use App\Models\User;
 
-
+use function Pest\Livewire\livewire;
 
 it('should render the list page', function () {
     Filament::setCurrentPanel(
         Filament::getPanel('admin'),
     );
 
-    // login with user
-    $user = User::factory()->create();
-
-    $this
-        ->actingAs($user)
-        ->get(ProjectResource::getUrl('index'))
+    livewire(ListProjects::class)
+        ->assertCanRenderTableColumn('title')
+        ->assertCanRenderTableColumn('created_at')
+        ->assertCanRenderTableColumn('actions')
         ->assertSuccessful();
 });
 
