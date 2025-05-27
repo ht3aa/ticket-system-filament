c0b369fc4ea673fdf3ab33364f91752e1c3c0ee6
ht3aa <ht3aa2001@gmail.com>
2025-05-27 22:57:18 +0300

    feat: add ProjectOverview widget to ListProjects page, enhancing project statistics display and integrating with the table for improved user insights

--- app/Filament/Admin/Resources/ProjectResource/Pages/ListProjects.php
+++ app/Filament/Admin/Resources/ProjectResource/Pages/ListProjects.php
@@ -3,17 +3,28 @@
 namespace App\Filament\Admin\Resources\ProjectResource\Pages;
 
 use App\Filament\Admin\Resources\ProjectResource;
+use App\Filament\Admin\Resources\ProjectResource\Widgets\ProjectOverview;
 use Filament\Actions;
+use Filament\Pages\Concerns\ExposesTableToWidgets;
 use Filament\Resources\Pages\ListRecords;
 
 class ListProjects extends ListRecords
 {
+    use ExposesTableToWidgets;
+
     protected static string $resource = ProjectResource::class;
 
     protected function getHeaderActions(): array
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            ProjectOverview::class,
         ];
     }
 }
